6c43d0e4a971790b0cd4cb41ef721825ae89ede3
Borcze Angelov <borce.angelov@yahoo.com>
2019-10-16 19:47:29 +0200

    Add RollsList UserControl + cleaner code

---
+++ Twr Windows UI/Component/RollsList.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using TryWinRoulette.Engine.Interface;
+
+namespace Twr_Windows_UI.Component
+{
+    /// <summary>
+    /// Interaction logic for RollsList.xaml
+    /// </summary>
+    public partial class RollsList : UserControl
+    {
+        public RollsList()
+        {
+            InitializeComponent();
+        }
+
+        public IRouletteRolls RouletteRolls
+        {
+            get { return (IRouletteRolls)GetValue(RouletteRollsProperty); }
+            set { SetValue(RouletteRollsProperty, value); }
+        }
+        
+        public static readonly DependencyProperty RouletteRollsProperty =
+            DependencyProperty.Register("RouletteRolls", typeof(IRouletteRolls), typeof(RollsList));
+    }
+}
--- Twr Windows UI/Component/Statistics.xaml.cs
+++ Twr Windows UI/Component/Statistics.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using TryWinRoulette.Engine.Interface;
 
 namespace Twr_Windows_UI.Component
@@ -31,10 +19,9 @@ namespace Twr_Windows_UI.Component
             get { return (IBasicStatistics)GetValue(BasicStatisticsProperty); }
             set { SetValue(BasicStatisticsProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for BasicStatistics.  This enables animation, styling, binding, etc...
+        
         public static readonly DependencyProperty BasicStatisticsProperty =
-            DependencyProperty.Register("BasicStatistics", typeof(IBasicStatistics), typeof(Statistics), new PropertyMetadata(null));
+            DependencyProperty.Register("BasicStatistics", typeof(IBasicStatistics), typeof(Statistics));
 
     }
 }
